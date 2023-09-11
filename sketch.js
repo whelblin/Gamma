dfaf13975711e74c166398faee1e7a1ef37f4c9b
whelblin <121453670+whelblin@users.noreply.github.com>
2023-09-11 14:43:19 -0400

    added timer

--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
+let timer;
 function setup() {
     new Canvas();
     // test circle
     let test = new Sprite(width/2, height/2, 50);
+    timer = new Timer();
   }
   
   function draw() {
     background(220);
+    console.log(timer.getCurrentTime());
+    text("Time: " + timer.getCurrentTime(), 10,20);
   }
--- timer.js
+++ timer.js
@@ -2,5 +2,13 @@
 // constructor should get the initial time
 // the getCurrentTime should return the current time - the initial time
 class Timer{
-    
+    constructor(){
+        this.timer = new Date();
+        this.initSec =this.timer.getTime()/1000;
+    }
+
+    getCurrentTime(){
+        let temp = new Date();
+        return temp.getTime()/1000 - this.initSec;
+    }
 }

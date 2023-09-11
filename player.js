634618f27fa4a839721149740e8a9ffdd306dc3b
Jhoope11 <jhoope11@kent.edu>
2023-09-11 19:32:33 -0400

    Organized index.html. Started player.js

--- player.js
+++ player.js
@@ -2,5 +2,7 @@
 // constructor should create the sprite and store it in a private attribute
 // methods will act on the sprite, such as movement
 class Player{
-
+    constructor(){
+        this.player = new Sprite(50, 50, 50, 50, 'static');
+    }
 };

40931d9d90816c9cef83617faf4eacad93d41d85
Jhoope11 <jhoope11@kent.edu>
2023-09-12 15:58:30 -0400

    Update Asteroid.js

---
+++ Asteroid.js
@@ -0,0 +1,10 @@
+// file for the asteroids class object
+// constructor should create the sprite and store it in a private attribute
+// methods will act on the sprite, such as spawning
+class Asteroid{
+    constructor(){
+        console.log(colliding);
+        this.Asteroid = new colliding.Sprite([20,30],[45,11],[80,20],[94,40],[80,93],[32,76],[20,80],[5,42],[24,34],[20,30]);
+
+    }
+};

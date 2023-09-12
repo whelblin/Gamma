a4181508b276915c43c12f823e5c610b33398348
Jhoope11 <jhoope11@kent.edu>
2023-09-11 21:03:27 -0400

    Implemented firing

--- player.js
+++ player.js
@@ -1,11 +1,16 @@
 // file for the player class object
 // constructor should create the sprite and store it in a private attribute
 // methods will act on the sprite, such as movement
+class Shot{
+    constructor(){
+        
+    }
+}
 class Player{
     constructor(){
         this.player = new Sprite();
         //this.player.diameter = 50;
-        
+        this.shot = new Sprite();
     }
     movement(){
         this.player.speed = 3;
@@ -27,5 +32,9 @@ class Player{
     }
     shoot(){
         //https://p5play.org/learn/sprite.html?page=2 moveTo:impulse
+        if( kb.pressing('space')){
+            bullet = new Shot();
+            bullet.moveTo(mouse,8);
+        }
     }
 };

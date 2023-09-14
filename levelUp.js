7e972e5dd5b5c04c67372e8d6fdf59031ce945ad
Jhoope11 <jhoope11@kent.edu>
2023-09-14 16:00:15 -0400

    Ground work established

---
+++ levelUp.js
@@ -0,0 +1,14 @@
+class LevelUp{
+    /*
+    This is how I'm evisioning this works; 
+        xp bar fills up
+            Game Pauses
+        Level Up box appears
+            Box has space for 3 to 4 items that are picked randomly from an array
+            After Item has been picked 
+                Box dissapears
+                Game Resumes
+                XP bar goes to zero
+    
+    */
+}

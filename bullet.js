c1ea7a934485ff9b6cb5dbeea69d6397adc5bb9e
whelblin <whelblin@kent.edu>
2023-09-13 21:11:37 -0400

    Update bullet.js

--- bullet.js
+++ bullet.js
@@ -4,7 +4,7 @@ class Bullet{
         this.bullet.diameter = 35;
         this.bullet.height = 50;
         this.bullet.width = 10;
-        this.bullet.life = 200;
+        this.bullet.life = 100;
 
         bullets.push(this.bullet);
     }

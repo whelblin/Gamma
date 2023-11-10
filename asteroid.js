720488a119fb5f1e33f7f0337f28adddd6ebb423
Ambersiaa <bigthomper67@gmail.com>
2023-11-10 16:19:50 -0500

    Added health pack

--- asteroid.js
+++ asteroid.js
@@ -24,6 +24,8 @@ class Asteroid{
             if(this.health <= 0){
                 removal(asteroids,this.asteroid)
                 ExpOrb.createExp(x, y, this.experericeAmount)
+                HealthPack.createHealth(x, y)
+                
             }
        }
        asteroids.push(this.asteroid);

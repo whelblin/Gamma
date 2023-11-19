d2d5e14decec173feef2a799742d0cc7b6b2ff11
Ambersiaa <bigthomper67@gmail.com>
2023-11-19 15:34:06 -0500

    fixed healthpack spawning

--- healthPack.js
+++ healthPack.js
@@ -6,7 +6,7 @@ class HealthPack {
      */
     // Basically the orb constructor minus the unique exp value
     constructor(x,y){
-        this.healthPack = new non_colliding.Sprite(x,y,20,);
+        this.healthPack = new non_colliding.Sprite(x,y,20);
         this.healthPack.addAnimation('small', 'assets/healthPack.png');
         this.healthPack.collides(player.returnPlayerObject())
         this.healthPack.overlaps(itmBoxes)
